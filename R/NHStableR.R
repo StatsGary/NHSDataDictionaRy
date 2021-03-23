2f39dc8260007f88225b3a6eb739df70f4323f61
Calum Polwart <cpolwart@chemo.org.uk>
2021-03-23 22:10:03 +0000

    Further imporove documentation, adding links.  Fix warnign that didn't actually action

--- R/NHStableR.R
+++ R/NHStableR.R
@@ -1,21 +1,24 @@
 
 #' NHStableR function
 #'
-#' This function uses the tableR parent function to return a table of elements, specifically from the NHS Data Dictionary
+#' This function uses the \code{\link{tableR}} parent function to return a table of elements, specifically from the NHS Data Dictionary
 #'
-#' @param data_element_name The data element name from https://datadictionary.nhs.uk/data_elements for which to return the table of elements, for example "ACCOMMODATION STATUS CODE"
+#' @param data_element_name The data element name from \url{https://datadictionary.nhs.uk/data_elements} for which to return the table of elements, for example "ACCOMMODATION STATUS CODE"
 #' @param ... Function forwarding to parent function to pass additional arguments to function (e.g. title, add_zero_prefix)
 #' @importFrom magrittr %>%
 #' @importFrom dplyr tibble
 #' @include tableR.R
 #' @return A tibble (class data.frame) output from the results of the web scrape
 #' \itemize{
-#'   \item result - the extracted html table from url and xpath passed
+#'   \item result - the extracted html table from the element page of the NHS Data Dictionary
 #'   \item DictType - defaults to Not Specified if nothing passed, however allows for custom dictionary / data frame tags to be created
 #'   \item DttmExtracted - a date and time stamp
 #' }
 #' @examples
 #' NHStableR("ACCOMMODATION STATUS CODE")
+#' # Returns a tibble with the Code and Description from https://datadictionary.nhs.uk/data_elements/accommodation_status_code.html
+#' # with Dict_Type added and a Date and Time the dictionary was extracted.
+#'
 #' @export
 #'
 
@@ -31,6 +34,7 @@ NHStableR <- function(data_element_name, ...){
 
   if ((data_element_name)!= toupper(data_element_name)){
     warning("The data element name is not all in uppercase. The element name has been capitalised to match the NHS datadictionary standard.")
+    data_element_name <- toupper(data_element_name)
   }
 
   nhs_data_lookup <- nhs_data_elements()
