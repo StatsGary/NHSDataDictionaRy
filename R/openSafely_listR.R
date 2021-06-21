d7a65376e650cd77d3640565c1bf5365ea7699f4
Gary Hutson <g.hutson@nhs.net>
2021-06-21 13:51:42 +0100

    Added fixes as per Uwe
    Thanks, we see:
    
       Found the following (possibly) invalid URLs:
         URL: https://codelists.opensafely.org/ (moved to
    https://www.opencodelists.org/)
           From: DESCRIPTION
                 man/openSafely_listR.Rd
                 inst/doc/introduction.html
           Status: 200
           Message: OK
         URL: https://codelists.opensafely.org/docs/ (moved to
    https://www.opencodelists.org/docs/)
           From: inst/doc/introduction.html
           Status: 200
           Message: OK
    
    Please change http --> https, add trailing slashes, or follow moved content as appropriate.
    
    Please fix and resubmit.
    
    Best,
    Uwe Ligges

--- R/openSafely_listR.R
+++ R/openSafely_listR.R
@@ -1,9 +1,9 @@
 #' openSafely_listR function
 #'
 #' This function uses the \code{\link{tableR}} parent function to return a table of elements, specifically from the OpenSafely Code List
-#' \url{https://codelists.opensafely.org/}
+#' \url{https://www.opencodelists.org/}
 #'
-#' @param list_name The code list ID from \url{https://codelists.opensafely.org/} for which to return the National table of elements, for example
+#' @param list_name The code list ID from \url{https://www.opencodelists.org/} for which to return the National table of elements, for example
 #' "opensafely/ace-inhibitor-medications"
 #' @param version The version of the code list if not the most recent
 #' @param ... Function forwarding to parent function to pass additional arguments to function (e.g. title, add_zero_prefix)
